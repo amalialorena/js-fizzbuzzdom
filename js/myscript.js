2ae30461b65fab66ae353be03e27d632376be011
Amalia <amalialorenaferariu@gmail.com>
2021-10-25 16:43:41 +0200

    fizzBuzz in page

--- js/myscript.js
+++ js/myscript.js
@@ -1,46 +1,33 @@
 // Scrivi un programma che esegua un ciclo da 1 a 100
 // e a ogni iterazione appenda un elemento html al container con uno stile differente a seconda del valore dell’indice
-
-
-
-
 // per i multipli di 3,
 // per i multipli di 5
-// e per i valori che sono sia multipli di 3 che di 5.
-
-
-
-
-
+// e per i valori che sono sia multipli di 3 che di 5
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Come faccio a sapere se un numero è divisibile per? 
-
-// let fizz = i % 3 == 0;
-// let buzz = i % 5 == 0;
-// let fizzBuzz = i % 3 == 0 && i % 3 == 0;
-
-
 // Come creare nuovi elementi html e appenderli al container?
 
-// let container = document.getElementById("container");
-// let square = document.createElement("div");
-// square.classList.add = ("square");
-// square.append(i);
-// container.append(square);
-
+let container = document.getElementById("container");
 
 for(let i = 1; i <= 100; i++ ) {
 
+    let square = document.createElement("div");
+    square.classList.add("square");
+    container.append(square);
+    
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log("fizzBuzz");
+        square.classList.add("fizzBuzz");
+        square.append("fizzBuzz");
     } else if (i % 3 == 0) {
-        console.log("fizz"); 
-    } else if (i % 5 == 0 )
-        console.log("buzz");
-    else {
-        console.log(i)
+        square.classList.add("fizz");
+        square.append("fizz");
+    } else if (i % 5 == 0 ) {
+        square.classList.add("buzz");
+        square.append("buzz");
+    } else {
+        container.append(square);
+        square.append(i)
     }  
-
 }
 
 
